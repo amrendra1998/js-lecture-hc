ae6b07f8661659ade09060d14ea4cebf1217cdcf
amrendra1998 <techupdate855@gmail.com>
2025-04-15 00:18:11 +0000

    final committed

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -6,3 +6,9 @@ accountCity = "jaipur"
 
 // accountId = 2
 console.log(accountId);
+accountEmail = "sonu@google.com"
+accountPassword = "4578946"
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountId , accountEmail , accountPassword , accountCity, accountState])
