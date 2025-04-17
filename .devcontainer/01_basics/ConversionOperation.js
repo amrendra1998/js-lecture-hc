72de893db3d8edfb8f2db693a156391ed45a8e98
amrendra1998 <techupdate855@gmail.com>
2025-04-17 17:58:25 +0000

    file comitted

---
+++ .devcontainer/01_basics/ConversionOperation.js
@@ -0,0 +1,27 @@
+// let score = true
+ 
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" ==> 33
+//  "33aaaa" ==> NaN
+// True ==>1 ; False ==>0 
+
+// let isLoggedIn = 5
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 ==> True; 0 ==>False
+// "" ==> False
+// "Sonu" ==> True
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
