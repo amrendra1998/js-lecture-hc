d98cb19bf949cd4181456eb1dc8ed894726c2325
amrendra1998 <techupdate855@gmail.com>
2025-04-19 18:09:04 +0000

    File Commited

--- .devcontainer/01_basics/ConversionOperation.js
+++ .devcontainer/01_basics/ConversionOperation.js
@@ -21,7 +21,45 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************Operation**************************
+
+let value = 3
+let Negvalue = -value
+// console.log(Negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**2);
+
+let str1 = "Amrendra"
+let str2 = " kumar"
+str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// let GameCounter = 100;
+// GameCounter++;
+// console.log(GameCounter);
+
+
+// Prefix Example 
+// let x = 5;
+// let y = ++x;
+// console.log(y);
+
+// Postfix Example 
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+
 
 
