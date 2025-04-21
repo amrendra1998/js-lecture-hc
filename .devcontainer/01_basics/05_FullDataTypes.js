88372211ccf395636d93b3202c949f54f41ba7e9
amrendra1998 <techupdate855@gmail.com>
2025-04-21 17:55:16 +0000

    Data Type Summmary

---
+++ .devcontainer/01_basics/05_FullDataTypes.js
@@ -0,0 +1,36 @@
+// Primitive Data Types --> Call by value 
+// 7 types : String, Number, Boolean, null, undefined, symbol , BigInt
+
+// Refrence (Non-Primitive)
+// Array, Objects, Functions 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outSideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol
+
+console.log(id == anotherId);
+
+
+// Refrence (Non-Primitive)
+// Array, objects, function 
+
+// const heros = ["shaktiman", "naagaj", "doga"]
+// let myObj={
+//     name = "Amrendra",
+//     age = 22,  
+// }
+
+const myFunction =  function() {
+    console.log("Hello World");
+
+}
+
+console.log(typeof(myFunction));
+
