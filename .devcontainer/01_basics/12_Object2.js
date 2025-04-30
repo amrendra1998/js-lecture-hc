4edc69e0677fd639caa666420b343b077bb618c9
amrendra1998 <techupdate855@gmail.com>
2025-04-30 00:24:28 +0000

    File commited to the

--- .devcontainer/01_basics/12_Object2.js
+++ .devcontainer/01_basics/12_Object2.js
@@ -27,14 +27,27 @@ const obj2 = { 4:"d", 5:"e", 6:"f"}
 // const obj3 = Object.assign(obj1, obj2);
 
 const obj3 = {...obj1, ...obj2};
-console.log(obj3);
+// console.log(obj3);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));
 
-console.log(Object.values(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(Object.hasOwnProperty('isAdmin'));
+// console.log(Object.hasOwnProperty('isAdmin'));
+
+// De-structuring of Object
+
+const course = {
+    courseName: "Javascript",
+    price : "999",
+    courseInstructore: "Sonu Kumar"
+}
+// course.courseInstructore
+
+const {courseInstructore : Instructor} = course;
+// console.log(courseInstructore);
+console.log(Instructor);
