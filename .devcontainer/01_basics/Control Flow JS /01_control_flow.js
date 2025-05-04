0dc2dc1b369004c0eca9b1738ff388e25f78888b
amrendra1998 <techupdate855@gmail.com>
2025-05-04 02:24:20 +0000

    file commited

---
+++ .devcontainer/01_basics/Control Flow JS /01_control_flow.js
@@ -0,0 +1,45 @@
+// if 
+// const isUserLoggedIn = true;
+const temperature = 30;
+
+
+// if (temperature === 40) {
+// // console.log("tempreature is high");
+// }
+// else {
+// // console.log("tempreature is low");
+// }
+// // console.log("Executed");
+
+const score = 200;
+if (score > 100) {
+    let power = "fly"
+    console.log(`use power: ${power}`);
+}
+
+// let balance  = 1000;
+// if(balance > 500) console.log("you are rich"), console.log("you are poor");
+
+// if(balance > 500) {
+//     console.log("balance is high");
+// } else if(balance > 200) {
+//     console.log("balance is medium");
+// } else if(balance > 100) {
+//     console.log("balance is low");
+// }
+// else {
+//     console.log("balance is very low");
+// }
+
+const isUserLoggedIn = true;
+const debitCard = true;
+const isUserLoggedInformGoogle = false;
+const isUserLoggedInformemail = true;
+
+if(isUserLoggedIn && debitCard && 2 == 3) {
+    console.log("You can book a ticket");
+}
+
+if(isUserLoggedIn || debitCard) {
+    console.log("Allowed to access course");
+}
